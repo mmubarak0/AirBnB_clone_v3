6c9b6f3c5837a79fcaf9e6f67a16deee4a6e2c96
Mohammed Mubarak <modymu9@gmail.com>
2024-01-05 18:52:28 +0200

    task 4 handling error page

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,16 @@ set up route for status endpoint
 
 
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status')
 def get_status():
     """ send status api"""
     return {"status": "OK"}
+
+
+@app_views.app_errorhandler(404)
+def handle_404(err):
+    """Handle 404 page not found error."""
+    return jsonify({"error": "Not found"})
