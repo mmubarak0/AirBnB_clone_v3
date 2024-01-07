437d97cd9398cac58e17bdd294ffdd2581700901
Mohammed Mubarak <modymu9@gmail.com>
2024-01-07 10:52:30 +0200

    Added Cross-Origin Resource Sharing (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ from api.v1.views import app_views
 from flask import Flask
 from models import storage
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
